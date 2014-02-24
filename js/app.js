eebd662d00f36b946c5ec1fcf754f9132ba1b7a1
sp <spirityy109@gmail.com>
2014-02-24 18:19:59 +0800

    update 2.25

--- js/app.js
+++ js/app.js
@@ -4,15 +4,49 @@ App.Router.map(function() {
     this.route('index',{path:'/'});
     this.route('search',{path:'/search'});
     this.route('cert',{path:'/cert'});
+
+    this.resource('category',{path:'category/:id'},function(){
+        this.route('category',{path:'/:id'}); 
+    }); 
+    this.resource('list',{path:'/:id'});
+    this.resource('detail',{path:'/:id'});
 });
 
 
-//App.ApplicationView = Ember.View.extend({
-//    templateName:'application1'
-//});
-//
 App.IndexRoute  = Ember.Route.extend({
-    setupController:function(){
-        console.info('index');
+    model:function(){
+        return indexlist; 
+    },
+
+});
+
+App.CategoryRoute = Ember.Route.extend({
+    model:function(params){
+        return categorylist.findBy('id',params.id); 
+    },
+    renderTemplate:function(){
+        this.render('category',{ outlet:'category' }); 
+    },
+    activate:function(){
+        $('.category').addClass('go-deep');
+    },
+    deactivate:function(){
+        $('.category').removeClass('go-deep');
+    }
+});
+
+App.CategoryView = Ember.View.extend({
+
+});
+
+App.ListRoute = Ember.Route.extend({
+    renderTemplate:function(){
+        this.render({outlet:'list'});    
+    }
+});
+
+App.DetailRoute = Ember.Route.extend({
+    renderTemplate:function(){
+        this.render({outlet:'detail'});    
     }
 });
---
+++ js/data.js
@@ -0,0 +1,52 @@
+var categorylist = [
+    {
+        id:'1',
+        value:'鲜果区'
+    },
+    {
+        id:'2',
+        value:'礼品区'
+    },
+    {
+        id:'3',
+        value:'人群'
+    },
+    {
+        id:'4',
+        value:'场合'
+    },
+    {
+        id:'5',
+        value:'品牌'
+    },
+    {
+        id:'6',
+        value:'原产地'
+    },
+    {
+        id:'7',
+        value:'限时抢购'
+    },
+];
+
+
+var indexlist  = [
+    {
+        id:'0001',
+        img_src:'http://static.fruitday.com/product_pic/2233/1/1-180x180-2233.jpg',
+        title:'佳沛新西兰金奇异果（S号）',
+        price:'89'
+    },
+    {
+        id:'0002',
+        img_src:'http://static.fruitday.com/product_pic/2233/1/1-180x180-2233.jpg',
+        title:'佳沛新西兰金奇异果（L号）',
+        price:'90'
+    },
+    {
+        id:'0003',
+        img_src:'http://static.fruitday.com/product_pic/2233/1/1-180x180-2233.jpg',
+        title:'香蕉dsada',
+        price:'98'
+    }
+];
